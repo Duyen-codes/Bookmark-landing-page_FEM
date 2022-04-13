be36f67d2cda44cf91a6bd64a5f49ce662c368b2
Duyen Nguyen <hongduyen0705@gmail.com>
2022-04-13 23:15:27 +0300

    added transition for mobile nav

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-const burgerIcon = document.querySelector(".burger-icon");
-const nav = document.querySelector("nav");
-const desktopLogo = document.querySelector(".desktop-logo");
-const closeIcon = document.querySelector(".close-icon");
+// const burgerIcon = document.querySelector(".burger-icon");
+// const nav = document.querySelector("nav");
+// const desktopLogo = document.querySelector(".desktop-logo");
+// const closeIcon = document.querySelector(".close-icon");
 
-burgerIcon.addEventListener("click", function () {
-  nav.style.display = "block";
-  desktopLogo.style.visibility = "hidden";
-});
+// burgerIcon.addEventListener("click", function () {
+//   nav.style.display = "block";
+//   burgerIcon.style.visibility = "hidden";
+//   desktopLogo.style.visibility = "hidden";
+// });
 
-closeIcon.addEventListener("click", function () {
-  nav.style.display = "none";
-  burgerIcon.style.visibility = "visible";
-  desktopLogo.style.visibility = "visible";
+// closeIcon.addEventListener("click", function () {
+//   nav.style.display = "none";
+//   burgerIcon.style.visibility = "visible";
+//   desktopLogo.style.visibility = "visible";
+// });
+
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+  console.log(visibility);
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
 });
